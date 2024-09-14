7621300b0f295c3ad5d13d16305229690c1143b3
narsi-chinnu <166526158+narsi2308@users.noreply.github.com>
2024-09-14 14:31:37 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "https://gbiyjb5k76.execute-api.ap-south-1.amazonaws.com/prod";
+var API_ENDPOINT = "https://ob2mt3xpck.execute-api.ap-south-1.amazonaws.com/prod";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
@@ -10,7 +10,7 @@ document.getElementById("savestudent").onclick = function(){
         "age": $('#age').val()
     };
     $.ajax({
-        url: "https://gbiyjb5k76.execute-api.ap-south-1.amazonaws.com/prod",
+        url: "https://ob2mt3xpck.execute-api.ap-south-1.amazonaws.com/prod",
         type: 'POST',
         data:  JSON.stringify(inputData),
         contentType: 'application/json; charset=utf-8',
@@ -26,7 +26,7 @@ document.getElementById("savestudent").onclick = function(){
 // AJAX GET request to retrieve all students
 document.getElementById("getstudents").onclick = function(){  
     $.ajax({
-        url: "https://gbiyjb5k76.execute-api.ap-south-1.amazonaws.com/prod",
+        url: "https://ob2mt3xpck.execute-api.ap-south-1.amazonaws.com/prod",
         type: 'GET',
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
